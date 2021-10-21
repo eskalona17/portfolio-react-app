c79cfe66476cbeeb99a881560ec63291c05a7b39
eskalona17 <eskalona_17@hotmail.com>
2021-10-21 15:47:49 +0200

    filters portfolio done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList"
 import "./Portfolio.scss"
+import {
+    featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    designPortfolio,
+    contentPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
-    const [selected, setSelected] = useState("featured")
+    const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([])
     const list = [
         {
             id: "featured",
@@ -26,12 +34,34 @@ export default function Portfolio() {
             title: "Branding"
         }
     ];
+
+    useEffect(() => {
+        switch (selected) {
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+            case "mobile":
+                setData(mobilePortfolio);
+                break;
+            case "design":
+                setData(designPortfolio);
+                break;
+            case "branding":
+                setData(contentPortfolio);
+                break;
+            default:
+                setData(featuredPortfolio);
+        }
+    }, [selected])
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
                 {list.map((item) => (
-                    <PortfolioList 
+                    <PortfolioList
                         title={item.title}
                         active={selected === item.id}
                         setSelected={setSelected}
@@ -39,26 +69,12 @@ export default function Portfolio() {
                 ))}
             </ul>
             <div className="container">
-                <div className="item">
-                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
-                    <h3>Banking App</h3>
-                </div>
+                {data.map((d) => (
+                    <div className="item">
+                        <img src={d.img} alt="" />
+                        <h3>{d.title}</h3>
+                    </div>
+                ))}
             </div>
         </div>
     )
