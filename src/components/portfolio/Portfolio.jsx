8a511fea4598fd6536e0584976c18e75b104ac1a
eskalona17 <eskalona_17@hotmail.com>
2021-10-21 14:15:41 +0200

    portfolio section done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,65 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList"
 import "./Portfolio.scss"
 
 export default function Portfolio() {
+    const [selected, setSelected] = useState("featured")
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "mobile",
+            title: "Mobile App"
+        },
+        {
+            id: "design",
+            title: "Design"
+        },
+        {
+            id: "branding",
+            title: "Branding"
+        }
+    ];
     return (
         <div className="portfolio" id="portfolio">
-            asdadsads
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((item) => (
+                    <PortfolioList 
+                        title={item.title}
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id={item.id} />
+                ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/web-design-g88236e6b3_640.jpg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
